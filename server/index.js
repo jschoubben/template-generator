9db1867f3acfccf838c14b9953ec82a73c0325ab
jschoubben <jschoubben@gmail.com>
2019-01-28 01:17:56 +0100

    added root package.json for running on azure

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const sqlGenerator = require('./sql-generator')
 const templateHelper = require('./template-helper')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('/', express.static('./client/public'));
 
