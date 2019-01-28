c9645a484969ab6c18c282f4bf1fa6d65b5c68b4
jschoubben <jschoubben@gmail.com>
2019-01-28 20:10:40 +0100

    removed template-gnerator because it may cause the error

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const appInsights = require("applicationinsights");
-const tg = require('./template-generator')
-const bodyParser = require('body-parser')
-const sqlGenerator = require('./sql-generator')
-const templateHelper = require('./template-helper')
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
 if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY){
     appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
     appInsights.start()
 } 
+
+const tg = require('./template-generator')
+const bodyParser = require('body-parser')
+const sqlGenerator = require('./sql-generator')
+//const templateHelper = require('./template-helper')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
 app.use('/', express.static('./client/public'));
 
@@ -20,13 +20,13 @@ app.post('/api/parse', async (req, res) => {
     res.send({data: result})
 })
 app.get('/api/templates', async (req, res) => {
-    const result = await templateHelper.getTemplates('./server/templates/')
-    res.send({data: result})
+    //const result = await templateHelper.getTemplates('./server/templates/')
+    res.send({data: {}})
 })
 
 app.get('/api/template', async (req, res) => {
-    const result = await templateHelper.getTemplate('./server/templates/' + req.query.template)
-    res.send({data: result})
+    //const result = await templateHelper.getTemplate('./server/templates/' + req.query.template)
+    res.send({data: {}})
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
