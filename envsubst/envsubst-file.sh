b15a853edf27851b88f2e02b291f7e6d7b2e8da8
jschoubben <jschoubben@gmail.com>
2019-02-03 16:52:00 +0100

    Added envsubst docker container for building purposes

--- envsubst/envsubst-file.sh
+++ envsubst/envsubst-file.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
-
+echo 'starting script!'
 FILENAME=$1
 FILENAME_NEW=$2
 
-if [ ! $FILENAME ]
-then
+echo $FILENAME
+echo $FILENAME_NEW
+
+if [ ! $FILENAME ]; then
   echo 'No filename argument provided'
   exit -1
 fi
-if [ ! $FILENAME2 ]
-then
+echo 'argument 1 provided'
+if [ ! -f "$FILENAME" ]; then
+    echo "File not found!"
+    exit -1
+fi
+if [ ! $FILENAME2 ]; then
   FILENAME_NEW=$FILENAME
 fi
 echo "Processing $FILENAME ..."
-
 envsubst < $FILENAME > $FILENAME-bak
 echo "Finished modifying file"
 mv $FILENAME-bak $FILENAME_NEW
