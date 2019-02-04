9f626f72c9094bd6b54c2160dc524d5d46857ce6
jschoubben <jschoubben@gmail.com>
2019-02-04 20:29:13 +0100

    Final version hopefully

--- server/config/production.js
+++ server/config/production.js
@@ -2,5 +2,6 @@ module.exports = {
   allowedOrigins: [
     'http://template-generator.azurewebsites.net',
     'https://template-generator.azurewebsites.net',
+    'http://' + process.env.MY_IP
   ]
 }
