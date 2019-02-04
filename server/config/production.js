4a8dcf83b25cd3daa325fd2d6a0db4e68c4d940d
jschoubben <jschoubben@gmail.com>
2019-02-05 00:29:55 +0100

    removed trailing slashes 😒

--- server/config/production.js
+++ server/config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   allowedOrigins: [
-    'http://template-generator.tk/',
-    'http://www.template-generator.tk/',
+    'http://template-generator.tk',
+    'http://www.template-generator.tk',
     'http://localhost'
   ]
 }
