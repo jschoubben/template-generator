c7d30cd06c59ce717bb461a9177f6ba32afd9cc2
jschoubben <jschoubben@gmail.com>
2019-02-07 00:02:24 +0100

    Updated api url

--- client/webpack/webpack.prod.config.js
+++ client/webpack/webpack.prod.config.js
@@ -7,7 +7,7 @@ module.exports = env => {
     plugins: [
       ...defaultConfig.plugins,
       new webpack.DefinePlugin({
-        "API_URL": JSON.stringify('http://template-generator.tk:3000/')
+        "API_URL": JSON.stringify('http://api.template-generator.tk/')
       })
     ],
     mode: 'production',
